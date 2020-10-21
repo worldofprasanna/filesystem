0d1550f8726efe978f73c7a65d7e1558776c4448
Prasanna <prasannacse2005@gmail.com>
2020-10-21 15:12:20 +0530

    add rm command

--- src/main/scala/com/tryout/files/Directory.scala
+++ src/main/scala/com/tryout/files/Directory.scala
@@ -9,6 +9,10 @@ class Directory(
                )
   extends DirEntry(parentPath, name) {
 
+  def removeEntry(entryName: String): Directory =
+    if (!hasEntry(entryName)) this
+    else new Directory(parentPath, name, contents.filter(x => !x.name.equals(entryName)))
+
   val isRoot: Boolean = parentPath.isEmpty
 
   def replaceEntry(entryName: String, newEntry: DirEntry): Directory =
@@ -34,6 +38,10 @@ class Directory(
   def findDescendant(path: List[String]): Directory =
     if (path.isEmpty) this
     else findEntry(path.head).asDirectory.findDescendant(path.tail)
+
+  def findDescendant(relativePath: String): Directory =
+    if (relativePath.isEmpty) this
+    else findDescendant(relativePath.split(Directory.SEPARATOR).toList)
 
   def hasEntry(name: String): Boolean = findEntry(name) != null
 
